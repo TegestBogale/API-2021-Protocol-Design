263d2d9c9223a883598edd4042e39d74814cd716
TegestBogale <tegest.bogale@heig-vd.ch>
2021-11-09 08:01:38 +0100

    client implementation

--- Client/src/main/java/ch/heigvd/api/calc/Client.java
+++ Client/src/main/java/ch/heigvd/api/calc/Client.java
@@ -1,7 +1,12 @@
 package ch.heigvd.api.calc;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,10 +23,15 @@ public class Client {
      * @param args no args required
      */
     public static void main(String[] args) {
+
         // Log output on a single line
         System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
 
         BufferedReader stdin = null;
+        BufferedReader in = null;
+        BufferedWriter out = null;
+        Socket clientSocket = null;
+
 
         /* TODO: Implement the client here, according to your specification
          *   The client has to do the following:
@@ -33,7 +43,48 @@ public class Client {
          *     - read the response line from the server (using BufferedReader.readLine)
          */
 
-        stdin = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            clientSocket = new Socket("127.0.0.1", 1111);
+            out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            stdin = new BufferedReader(new InputStreamReader(System.in));
+
+            String userInput;
+            while((userInput = stdin.readLine()).equals("QUIT CRLF")){
+                out.write(userInput);
+                out.flush();
+                LOG.log(Level.INFO, "*** Response sent by the server: ***");
+                String response;
+                while ((response = in.readLine()) != null) {
+                    LOG.log(Level.INFO, response);
+                }
+            }
+
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, ex.toString(), ex);
+        } finally {
+            try {
+                if (out != null)
+                    out.close();
+            } catch (IOException ex) {
+                LOG.log(Level.SEVERE, ex.toString(), ex);
+            }
+            try {
+                if (stdin != null)
+                    out.close();
+            } catch (IOException ex) {
+                LOG.log(Level.SEVERE, ex.toString(), ex);
+            }
+            try {
+                if (clientSocket != null && ! clientSocket.isClosed())
+                    clientSocket.close();
+            } catch (IOException ex) {
+                LOG.log(Level.SEVERE, ex.toString(), ex);
+            }
+        }
+
+
+
 
     }
 }
